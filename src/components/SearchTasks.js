574e0c92336bf00b4d4a4505f6eae0cfd384b7a7
Jakub <jakub.matwis@gmail.com>
2022-03-01 23:02:18 +0100

    observer done

--- src/components/SearchTasks.js
+++ src/components/SearchTasks.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Task from "./Task";
 
 export default function SearchTasks({ tasks, setTasks }) {
@@ -6,16 +6,16 @@ export default function SearchTasks({ tasks, setTasks }) {
   const handleSearch = (e) => {
     setSearch(e.target.value);
   };
+
+  const filterTasks = (tasks) => tasks.filter((task) => `${task.title} ${task.description}`.toLowerCase().includes(search.toLocaleLowerCase()));
   return (
     <section className="section">
       <h2 className="section__headline">Search Tasks:</h2>
       <input onChange={handleSearch} type="text" className="section__input--search" />
       <ul className="section__list">
-        {tasks
-          .filter((task) => `${task.title} ${task.description}`.toLowerCase().includes(search.toLocaleLowerCase()))
-          .map((task, index) => (
-            <Task key={index} setTasks={setTasks} task={task} />
-          ))}
+        {filterTasks(tasks).map((task, index) => (
+          <Task key={index} setTasks={setTasks} task={task} />
+        ))}
       </ul>
     </section>
   );
--- src/components/Task.js
+++ src/components/Task.js
@@ -3,32 +3,32 @@ import { HiOutlineTrash } from "react-icons/hi";
 import { GoPerson, GoBriefcase } from "react-icons/go";
 
 export default function Task({ setTasks, task }) {
-  const [isVisible, setIsVisible] = useState(false);
+  const [taskClass, setTaskClass] = useState("task");
   const targetRef = useRef(null);
 
   const observerCallback = (entries) => {
     const [entry] = entries;
-    console.log("-------------------");
-    console.log(entries);
-    console.log(entry);
-    console.log(entry.isIntersecting);
-    console.log("-------------------");
 
-    setIsVisible(entry.isIntersecting);
+    if (entry.className !== "task show") {
+      if (entry.isIntersecting) setTaskClass("task show");
+    }
   };
 
   const observerOptions = useMemo(() => {
     return {
       root: null,
       rootMargin: "0px",
-      threshold: 0.3,
+      threshold: 0,
     };
   }, []);
   useEffect(() => {
     const observer = new IntersectionObserver(observerCallback, observerOptions);
     const currentTarget = targetRef.current;
-    if (currentTarget) observer.observe(currentTarget);
-  }, [observerOptions]);
+    if (currentTarget) {
+      observer.observe(currentTarget);
+    }
+    return () => observer.unobserve(currentTarget);
+  });
   const iconDictionary = {
     work: <GoBriefcase className="task__title__icon" size="1.6rem" />,
     private: <GoPerson className="task__title__icon" size="1.6rem" />,
@@ -39,7 +39,7 @@ export default function Task({ setTasks, task }) {
     setTasks((tasks) => tasks.filter((item) => item.id !== task.id));
   };
   return (
-    <li className={!isVisible ? "task" : "task show"} ref={targetRef}>
+    <li className={taskClass} ref={targetRef}>
       <div className="task__title">
         <TaskIcon category={task.category} />
         <h3>{task.title}</h3>
